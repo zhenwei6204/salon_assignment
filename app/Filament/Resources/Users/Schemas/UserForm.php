8424403a95222a43c9328b9e378742dff5507c69
zhenwei6204 <zhenwei6204@gmail.com>
2025-08-28 22:32:57 +0800

    NEW

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                DateTimePicker::make('email_verified_at'),
+                TextInput::make('password')
+                    ->password()
+                    ->required(),
+                Textarea::make('two_factor_secret')
+                    ->default(null)
+                    ->columnSpanFull(),
+                Textarea::make('two_factor_recovery_codes')
+                    ->default(null)
+                    ->columnSpanFull(),
+                DateTimePicker::make('two_factor_confirmed_at'),
+            ]);
+    }
+}
