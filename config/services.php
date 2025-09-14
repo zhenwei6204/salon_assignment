0c52ff7af795b976455dcea3c9422fbd9eb28abb
hueyren0303 <hueyren03@gmail.com>
2025-09-14 17:09:13 +0800

    HUEYREN WEB SERVICE (COMPLETE)

--- config/services.php
+++ config/services.php
@@ -23,6 +23,9 @@ return [
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
+     'resend' => [
+        'key' => env('RESEND_KEY'),
+    ],
 
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
